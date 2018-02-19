47e2e84bc8e4156f0071c06755b6068e2a9074ff
gudvinga <gudvinga@gmail.com>
2018-02-20 01:58:10 +0300

    work done /need refactoring

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let count = 0;
-  for (let i = 0; i < preferences.length; i += 3) {
-    if (i + 1 === preferences[preferences[preferences[i] - 1] - 1]) {
+  for (let i = 0; i < preferences.length; i++) {
+    if (i + 1 === preferences[preferences[preferences[i] - 1] - 1] && preferences[i] !== preferences[preferences[i] - 1] ) {
       count++;
+      delete preferences[i];
+      delete preferences[preferences[i] - 1];
+      delete preferences[preferences[preferences[i] - 1] - 1];
     }
   }
   return count;
